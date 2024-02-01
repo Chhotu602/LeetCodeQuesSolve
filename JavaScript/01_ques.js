b7d20a9e10a9efa727574596ecb790fe391677e1
Eklavya <141182144+Chhotu602@users.noreply.github.com>
2024-02-01 02:58:57 +0000

    daily_temp

---
+++ JavaScript/01_ques.js
@@ -0,0 +1,13 @@
+function createCounter(n) {
+    return function() {
+        return n++;
+    };
+}
+
+// Example usage:
+const myCounter = createCounter(5);
+
+console.log(myCounter()); // Output: 5
+console.log(myCounter()); // Output: 6
+console.log(myCounter()); // Output: 7
+// and so on...
